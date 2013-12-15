9c2d9cbf669e61b65203de539743381b3517a4b5
de3sw2aq1 <de3sw2aq1@invalid.invalid>
2013-12-15 09:57:56 +0000

    Ra ebook scraper

---
+++ build.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+import ez_epub
+from lxml import html
+from lxml.etree import Comment
+import logging
+import json
+from sys import exit
+# from genshi.template import TemplateLoader
+from genshi.input import HTML
+
+# import IPython
+# from pprint import pprint
+
+class Chapter:
+
+    def __init__(self):
+        self.title = ''
+        self.subsections = []
+        self.css = ''
+        self.text = []
+        self.templateFileName = 'chapter.html'
+
+
+JSON_PATH = 'scrape.json'
+
+
+logging.basicConfig()
+logger = logging.getLogger("scraper")
+logger.setLevel(logging.DEBUG)
+
+with open(JSON_PATH, 'r') as outfile:
+	pages = json.load(outfile)
+
+
+book = ez_epub.Book()
+book.title = "Ra"
+book.authors = ["Sam Hughes"]
+book.sections = []
+book.impl.add_meta('source', 'http://qntm.org/ra')
+
+for page in pages:
+	print
+	print page['title']
+	print page['url']
+
+	content = html.fromstring(page['text'])
+
+	# Convert contents to actual tags
+	# for c in content.iter(Comment):
+	# 	logger.warn('Converting commment to tag' )
+	# 	# logger.debug('Comment text:' + c.text)
+	# 	pre = content.makeelement('pre', {'class': 'converted-comment'})
+	# 	pre.text = c.text
+	# 	content.replace(c, pre)
+
+	# Convert style="text-align: right" to class
+	for tag in content.xpath("//*[starts-with(@style, 'text-align: right')]"):
+		logger.debug('Converting "text-align: right" to class' )
+		del tag.attrib['style']
+		tag.attrib['class'] = 'text-right'
+
+	# Convert style="text-align: center" to class
+	for tag in content.xpath("//*[starts-with(@style, 'text-align: center')]"):
+		logger.debug('Converting "text-align: center" to class' )
+		del tag.attrib['style']
+		tag.attrib['class'] = 'text-center'
+
+	# Check for missed style attributes
+	for tag in content.xpath("//*[@style]"):
+		logger.warn('Found remaining style attribute' )
+		# IPython.embed()
+		exit('Giving up')
+
+	# IPython.embed()
+
+	chapter = Chapter()
+	chapter.html = HTML(html.tostring(content), encoding='utf-8')
+	chapter.title = page['title']
+	book.sections.append(chapter)
+
+book.make(book.title + '.epub')
---
+++ scrape.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+import requests
+from lxml import html
+from lxml.etree import Comment
+import logging
+import json
+from sys import exit
+from urlparse import urljoin
+
+FIRST_URL = 'http://qntm.org/city'
+JSON_PATH = 'scrape.json'
+
+def get(url):
+	r = requests.get(url)
+	return html.fromstring(r.text)
+
+def getNextUrl(content, url):
+	next_h4 = content.xpath('h4')[-1]
+	if next_h4.text == 'Next: ':
+		content.remove(next_h4)
+		return urljoin(url, next_h4[0].get('href'))
+	elif next_h4.text == 'To be continued':
+		return None
+	else:
+		logger.warn('Unkown last h4')
+		# IPython.embed()
+		exit('Giving up')
+
+logging.basicConfig()
+logger = logging.getLogger("scraper")
+logger.setLevel(logging.DEBUG)
+
+url = FIRST_URL
+i = 1
+
+try:
+	with open(JSON_PATH, 'r') as outfile:
+		pages = json.load(outfile)
+except IOError, e:
+	pages = []
+
+if pages:
+	url = pages[-1]['url']
+	i = pages[-1]['id'] + 1
+
+	logger.info('Requesting: ' + url)
+	src = get(url)
+	logger.info('Looking for new pages')
+	url = getNextUrl(src.xpath("//div[@id='content']")[0], url)
+	if not url:
+		exit("No new pages")
+
+print "Starting"
+
+while url:
+	logger.info('Requesting: ' + url)
+	src = get(url)
+	content = src.xpath("//div[@id='content']")[0]
+	title = src.xpath("//h2")[0]
+
+	# Convert contents to actual tag
+	for c in content.iter(Comment):
+		logger.warn('Detected commment' )
+		logger.debug('Comment text:' + c.text)
+
+	url = getNextUrl(content, url)
+
+	try:
+		prev_h4 = content.xpath('h4')[0]
+		if prev_h4[0].tag == 'a' and prev_h4[0].text == 'Previously':
+			content.remove(prev_h4)
+	except IndexError, e:
+		pass
+
+	page = {'id': i, 'url': url, 'title': title.text, 'text': html.tostring(content)}
+
+	logger.info('Processed: ' + title.text)
+	pages.append(page)
+
+	if i % 100 == 0:
+		logger.info('Dumping json')
+		with open(JSON_PATH, 'w') as outfile:
+			json.dump(pages, outfile)
+
+	i += 1
+
+with open(JSON_PATH, 'w') as outfile:
+  json.dump(pages, outfile)
+
+print "Done"
