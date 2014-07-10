3c517d11e9d3f55af080b63f99bbcb344e615655
de3sw2aq1 <de3sw2aq1@invalid.invalid>
2014-07-10 03:38:37 -0500

    Add --convert-comments flag to ./build.py
    Either always strip comments during building, or convert them into <pre> tags.
    
    Also convert tabs to spaces in build.py

--- build.py
+++ build.py
@@ -5,8 +5,8 @@ from lxml import html
 from lxml.etree import Comment
 import logging
 import json
-from sys import exit
 from genshi.input import HTML
+import sys
 
 class Chapter:
 
@@ -20,13 +20,19 @@ class Chapter:
 
 JSON_PATH = 'scrape.json'
 
+# TODO: use argparse?
+convert_comments = False
+if len(sys.argv) == 2 and sys.argv[1] == '--convert-comments':
+    convert_comments = True
+elif len(sys.argv) > 1:
+    sys.exit('USAGE: {} [--convert-comments]'.format(sys.argv[0]))
 
 logging.basicConfig()
 logger = logging.getLogger("scraper")
 logger.setLevel(logging.DEBUG)
 
 with open(JSON_PATH, 'r') as outfile:
-	pages = json.load(outfile)
+    pages = json.load(outfile)
 
 
 book = ez_epub.Book()
@@ -36,40 +42,43 @@ book.sections = []
 book.impl.add_meta('source', 'http://qntm.org/ra')
 
 for page in pages:
-	print
-	print page['title']
-	print page['url']
+    print
+    print page['title']
+    print page['url']
 
-	content = html.fromstring(page['text'])
+    content = html.fromstring(page['text'])
 
-	# Convert contents to actual tags
-	# for c in content.iter(Comment):
-	# 	logger.warn('Converting commment to tag' )
-	# 	# logger.debug('Comment text:' + c.text)
-	# 	pre = content.makeelement('pre', {'class': 'converted-comment'})
-	# 	pre.text = c.text
-	# 	content.replace(c, pre)
+    # Convert contents to actual tags or remove them
+    for c in content.iter(Comment):
+        if convert_comments:
+            logger.warn('Converting commment to tag' )
+            pre = content.makeelement('pre', {'class': 'converted-comment'})
+            pre.text = c.text
+            content.replace(c, pre)
+        else:
+            logger.warn('Removing commment' )
+            c.getparent().remove(c)
 
-	# Convert style="text-align: right" to class
-	for tag in content.xpath("//*[starts-with(@style, 'text-align: right')]"):
-		logger.debug('Converting "text-align: right" to class' )
-		del tag.attrib['style']
-		tag.attrib['class'] = 'text-right'
+    # Convert style="text-align: right" to class
+    for tag in content.xpath("//*[starts-with(@style, 'text-align: right')]"):
+        logger.debug('Converting "text-align: right" to class' )
+        del tag.attrib['style']
+        tag.attrib['class'] = 'text-right'
 
-	# Convert style="text-align: center" to class
-	for tag in content.xpath("//*[starts-with(@style, 'text-align: center')]"):
-		logger.debug('Converting "text-align: center" to class' )
-		del tag.attrib['style']
-		tag.attrib['class'] = 'text-center'
+    # Convert style="text-align: center" to class
+    for tag in content.xpath("//*[starts-with(@style, 'text-align: center')]"):
+        logger.debug('Converting "text-align: center" to class' )
+        del tag.attrib['style']
+        tag.attrib['class'] = 'text-center'
 
-	# Check for missed style attributes
-	for tag in content.xpath("//*[@style]"):
-		logger.warn('Found remaining style attribute' )
-		exit('Giving up')
+    # Check for missed style attributes
+    for tag in content.xpath("//*[@style]"):
+        logger.warn('Found remaining style attribute' )
+        sys.exit('Giving up')
 
-	chapter = Chapter()
-	chapter.html = HTML(html.tostring(content), encoding='utf-8')
-	chapter.title = page['title']
-	book.sections.append(chapter)
+    chapter = Chapter()
+    chapter.html = HTML(html.tostring(content), encoding='utf-8')
+    chapter.title = page['title']
+    book.sections.append(chapter)
 
 book.make(book.title + '.epub')
